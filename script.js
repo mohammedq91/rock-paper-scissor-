1f3399abbc2c18898a1014f00ac8b5d26d1bb345
Mohammed <mohammedq91@gmail.com>
2021-05-05 16:48:48 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+//place rock, paper, scissors,
+var rock = document.getElementsClassName('item-rock').addEventListener('click',play)
+var paper = document.getElementsByClassName('.item-paper').addEventListener('click', play)
+var scissor = document.getElementsByClassName('.item-scissor').addEventListener('click', play)
+
+
+
+function play(){
+  console.log('You clicked me!')
+};
+
+
+
