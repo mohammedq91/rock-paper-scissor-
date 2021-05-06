32ae76e5ef2187fa2fd1d6e0cd8d0a5169dc484f
Mohammed <mohammedq91@gmail.com>
2021-05-06 12:19:26 -0400

    js file

--- script.js
+++ script.js
@@ -1,25 +1,15 @@
-//place rock, paper, scissors,
-
 var rock = document.getElementById('rock').addEventListener('click',play);
 var paper = document.getElementById('paper').addEventListener('click', play);
 var scissor = document.getElementById('scissor').addEventListener('click', play);
-var userChoies = prompt("do u choice")
-
-var playerChoice;
-var computerChoice;
-
 
 function play(e){
   console.log('You clicked me!')
   e.preventDefault()
   computerPlay();
-  userPlayer()
-  
 };
 
 function computerPlay(){
   var randomNumber = Math.random();
-  console.log(randomNumber)
 
   if (randomNumber <= 0.33){
     computerChoice = "rock";
