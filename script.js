e692d48c799ef2b0aea601d67150dc12435d7e42
Mohammed <mohammedq91@gmail.com>
2021-05-07 11:53:22 -0400

    computer choice added

--- script.js
+++ script.js
@@ -1,22 +1,41 @@
-var rock = document.getElementById('rock').addEventListener('click',play);
-var paper = document.getElementById('paper').addEventListener('click', play);
-var scissor = document.getElementById('scissor').addEventListener('click', play);
+const rock = document.getElementById('rock').addEventListener('click', play)
+const paper = document.getElementById('paper').addEventListener('click', play)
+const scissors = document.getElementById('scissors').addEventListener('click', play)
+
+const userChoiceDisplay = document.getElementById('user-play-display')
+const computerChoiceDisplay = document.getElementById('computer-play-display')
+
+const user = document.getElementById('user')
+const computer = document.getElementById('computer')
+const outcome = document.getElementById('outcome')
+
 
 function play(e){
-  console.log('You clicked me!')
-  e.preventDefault()
+  const rock = e.target.id;
+  userChoiceDisplay.textContent = "You have chosen: " + rock
+
+  const paper = e.target.id 
+  userChoiceDisplay.textContent = "You have chosen: " + paper
+
+  const scissors = e.target.id
+  userChoiceDisplay.textContent = "You have chosen: " + scissors
+
   computerPlay();
-};
+}
 
 function computerPlay(){
   var randomNumber = Math.random();
 
   if (randomNumber <= 0.33){
-    computerChoice = "rock";
+    computerChoiceDisplay.textContent = "Computer choses rock";
+
   } else if (randomNumber > 0.33 && randomNumber <= 0.66){
-    computerChoice = "paper";
+    computerChoiceDisplay.textContent = "Computer choses paper";
+
   } else 
-    computerChoice = "scissor"
-};
+    computerChoiceDisplay.textContent = "Computer choses scissors";
+  };
+  
+
 
 
