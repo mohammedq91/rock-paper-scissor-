f2c6cc4e1b2d15a7d4aa96d63799bc96620374ef
Mohammed <mohammedq91@gmail.com>
2021-06-25 14:39:06 -0400

    Final JS review

--- reviewedScript.js
+++ reviewedScript.js
@@ -3,44 +3,48 @@ for (gameBtn of gameBtns){
   gameBtn.addEventListener("click", (e) => userPlay(e));
 }
 
-const outcome = document.getElementById('outcome')
+function userPlay(e){
+  const user = e.target.id;
 
-
-fucntion userPlay(e){
-  const user = e.target.id
-  const userDisplay = document.getElementById('user')
+  const userDisplay = document.getElementById('user');
   userDisplay.textContent = `You have chosen: ${user}`;
 
   result(user);
-}
+};
 
 function computerPlay(){
-  const choices = ["rock", "paper", "scissors"]
-  return choices[Math.floor(Math.random() * choices.length)]
+  const choices = ["rock", "paper", "scissors"];
+  return choices[Math.floor(Math.random() * choices.length)];
 }
-
 
 function result(user){
   const computer = computerPlay();
-  const computerDisplay = document.getElementById('computer')
-  compupterDisplay.textContent = `Computer chooses: ${computer}`
+
+  const computerDisplay = document.getElementById('computer');
+  computerDisplay.textContent = `Computer chooses: ${computer}`;
+
+  if (user === computer) displayResult('tie');
 
   switch (user) {
     case "rock":
-      if (computer ===)
-      
+      if (computer === 'paper') displayResult("lost");
+      if (computer === "scissors") displayResult("won");
       break;
     case "paper":
-      
+      if (computer === "rock") displayResult("won");
+      if (computer === "scissors") displayResult("lost");
       break;
-    case "rock":
-      
-      break;
-  
-    default:
+    case "scissors":
+      if (computer === "paper") displayResult("won");
+      if (computer === "rock") displayResult("lost");
       break;
   }
 
+  function displayResult(result){
+    const outcome = document.getElementById('outcome');
 
-
+    if (result === "won" || "lost") outcome.textContent = `You ${result}!`;
+    if (result === "tie") outcome.textContent = `You have a ${result};`;
+  }
 }
+
